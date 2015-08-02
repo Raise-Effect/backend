16dd0aed9d2e63b301f22183fd58bdfb1216566a
Evan Palmer <evjpalmer@gmail.com>
2015-08-02 13:42:11 -0700

    test with dummy data

--- jobs-economy/app/api/controllers.py
+++ jobs-economy/app/api/controllers.py
@@ -1,8 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from flask import current_app as app
 
 api = Blueprint('api', __name__, url_prefix='/')
 
 @api.route('/', methods=['GET'])
-def index():        
+def index():
     return 'Hello world'
+
+@api.route('county', methods=['GET'])
+def county_data():
+    data = {
+        "population": 1000,
+        "adults": 350,
+        "children": 650,
+        "laborforce": 900,
+        "employed": 800,
+        "unemployed": 100
+    }
+    return jsonify(data)
