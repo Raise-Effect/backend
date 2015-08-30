4f7700a0eceb51fdf406845fb0a25aadddceb5b1
Gareth Ferneyhough <gareth@space.vu>
2015-08-30 12:43:32 -0700

    Added href field to counties.

--- jobs-economy/app/api/controllers.py
+++ jobs-economy/app/api/controllers.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from flask import current_app as app
+from flask import current_app as app, request
 import models
 
 api = Blueprint('api', __name__, url_prefix='/api/v1/')
@@ -11,4 +11,7 @@ def index():
 
 @api.route('counties/<int:fips>', methods=['GET'])
 def counties(fips):
-    return models.County.query.filter_by(fips = fips).first().county
+    return jsonify({
+        'href': request.path,
+        'name': models.County.query.filter_by(fips = fips).first().county
+        })
