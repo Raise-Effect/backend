d930638e0530fa2f2b4618b7862e3e6205979761
Evan Palmer <evjpalmer@gmail.com>
2015-09-13 12:39:38 -0700

    correct minor typos

--- jobs-economy/app/api/controllers.py
+++ jobs-economy/app/api/controllers.py
@@ -49,7 +49,7 @@ def wagestats_for_county(fips):
     return query.construct_wagestats_for_county(fips)
 
 @api.route('counties/calculatedstats', methods=['GET'])
-def calcalatedstats_all():
+def calculatedstats_all():
     return query.construct_calculatedstats_all()
 
 @api.route('counties/<int:fips>/calculatedstats', methods=['GET'])
@@ -70,7 +70,7 @@ def ssscredits_all():
 
 @api.route('counties/<int:fips>/ssscredits', methods=['GET'])
 def ssscredits_for_county():
-    return query.construct_ssscredits_for_county()
+    return query.construct_ssscredits_for_county(fips)
 
 @api.route('counties/ssswages', methods=['GET'])
 def ssswages_all():
@@ -78,7 +78,7 @@ def ssswages_all():
 
 @api.route('counties/<int:fips>/ssswages', methods=['GET'])
 def ssswages_for_county():
-    return query.construct_ssswages_for_county()
+    return query.construct_ssswages_for_county(fips)
 
 @api.route('counties/puma', methods=['GET'])
 def puma_all():
@@ -86,7 +86,7 @@ def puma_all():
 
 @api.route('counties/<int:fips>/puma', methods=['GET'])
 def puma_for_county():
-    return query.construct_puma_for_county()
+    return query.construct_puma_for_county(fips)
 
 @api.errorhandler(404)
 def not_found(error):
