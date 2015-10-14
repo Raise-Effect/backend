587416e71864ec51ec019f430f26aa838ddf4bf7
Evan Palmer <evjpalmer@gmail.com>
2015-10-14 14:03:24 -0700

    minor formatting fix in query.py

--- jobs-economy/app/api/query.py
+++ jobs-economy/app/api/query.py
@@ -90,18 +90,18 @@ def construct_laborstats_for_county(fips):
 def construct_population_all():
     return [
         {
-          "fips": stat.fips,
-          "population": stat.population,
-          "adults": stat.adults,
-          "kids": stat.kids,
-          "kidspresentc": stat.kidspresentc,
-          "a1cC": stat.a1cc,
-          "a2s2C": stat.a2s2c,
-          "a1c0C": stat.a1c0c,
-          "a1teenC": stat.a1teenc,
-          "mindiff": stat.mindiff,
-          "mostcommonfamilytype": stat.mostcommonfamilytype,
-          "year": stat.year
+            "fips": stat.fips,
+            "population": stat.population,
+            "adults": stat.adults,
+            "kids": stat.kids,
+            "kidspresentc": stat.kidspresentc,
+            "a1cC": stat.a1cc,
+            "a2s2C": stat.a2s2c,
+            "a1c0C": stat.a1c0c,
+            "a1teenC": stat.a1teenc,
+            "mindiff": stat.mindiff,
+            "mostcommonfamilytype": stat.mostcommonfamilytype,
+            "year": stat.year
         } for stat in models.Population.query]
 
 
@@ -109,18 +109,18 @@ def construct_population_all():
 def construct_population_for_county(fips):
     stat = models.Population.query.filter_by(fips=fips).first_or_404()
     return {
-          "fips": stat.fips,
-          "population": stat.population,
-          "adults": stat.adults,
-          "kids": stat.kids,
-          "kidspresentc": stat.kidspresentc,
-          "a1cC": stat.a1cc,
-          "a2s2C": stat.a2s2c,
-          "a1c0C": stat.a1c0c,
-          "a1teenC": stat.a1teenc,
-          "mindiff": stat.mindiff,
-          "mostcommonfamilytype": stat.mostcommonfamilytype,
-          "year": stat.year
+        "fips": stat.fips,
+        "population": stat.population,
+        "adults": stat.adults,
+        "kids": stat.kids,
+        "kidspresentc": stat.kidspresentc,
+        "a1cC": stat.a1cc,
+        "a2s2C": stat.a2s2c,
+        "a1c0C": stat.a1c0c,
+        "a1teenC": stat.a1teenc,
+        "mindiff": stat.mindiff,
+        "mostcommonfamilytype": stat.mostcommonfamilytype,
+        "year": stat.year
     }
 
 
@@ -139,6 +139,7 @@ def construct_familytype_all():
             "teenagers": stat.teenagers,
             "children": stat.children
         } for stat in models.FamilyType.query]
+
 
 @jsonify_lru_cache()
 def construct_wagestats_all():
