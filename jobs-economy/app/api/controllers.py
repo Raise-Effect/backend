61e1c1f8b6597d8444105966704b89cb3443005b
Gareth Ferneyhough <gareth@space.vu>
2015-10-17 14:48:57 -0700

    removed trailing slashes from routes. was messing up CORS

--- jobs-economy/app/api/controllers.py
+++ jobs-economy/app/api/controllers.py
@@ -56,128 +56,128 @@ def index():
     <a href="https://github.com/Jobs-Economy/backend">here</a>"""
 
 
-@api.route('counties/', methods=['GET'])
+@api.route('counties', methods=['GET'])
 @crossdomain(origin='*')
 def counties_all():
     return query.construct_counties()
 
 
-@api.route('counties/<int:fips>/', methods=['GET'])
+@api.route('counties/<int:fips>', methods=['GET'])
 @crossdomain(origin='*')
 def county(fips):
     return query.construct_county(fips)
 
 
-@api.route('counties/laborstats/', methods=['GET'])
+@api.route('counties/laborstats', methods=['GET'])
 @crossdomain(origin='*')
 def laborstats_all():
     return query.construct_laborstats_all()
 
 
-@api.route('counties/<int:fips>/laborstats/', methods=['GET'])
+@api.route('counties/<int:fips>/laborstats', methods=['GET'])
 @crossdomain(origin='*')
 def laborstats_for_county(fips):
     return query.construct_laborstats_for_county(fips)
 
 
-@api.route('counties/population/', methods=['GET'])
+@api.route('counties/population', methods=['GET'])
 @crossdomain(origin='*')
 def population_all():
     return query.construct_population_all()
 
 
-@api.route('counties/<int:fips>/population/', methods=['GET'])
+@api.route('counties/<int:fips>/population', methods=['GET'])
 @crossdomain(origin='*')
 def population_for_county(fips):
     return query.construct_population_for_county(fips)
 
 
-@api.route('counties/familytype/', methods=['GET'])
+@api.route('counties/familytype', methods=['GET'])
 @crossdomain(origin='*')
 def familytype_all():
     return query.construct_familytype_all()
 
 
-@api.route('counties/wagestats/', methods=['GET'])
+@api.route('counties/wagestats', methods=['GET'])
 @crossdomain(origin='*')
 def wagestats_all():
     return query.construct_wagestats_all()
 
 
-@api.route('counties/<int:fips>/wagestats/', defaults={'year': 2013}, methods=['GET'])
-@api.route('counties/<int:fips>/wagestats/<int:year>/', methods=['GET'])
+@api.route('counties/<int:fips>/wagestats', defaults={'year': 2013}, methods=['GET'])
+@api.route('counties/<int:fips>/wagestats/<int:year>', methods=['GET'])
 @crossdomain(origin='*')
 def wagestats_for_county(fips, year):
     return query.construct_wagestats_for_county(fips, year)
 
 
-@api.route('counties/calculatedstats/', methods=['GET'])
+@api.route('counties/calculatedstats', methods=['GET'])
 @crossdomain(origin='*')
 def calculatedstats_all():
     return query.construct_calculatedstats_all()
 
 
-@api.route('counties/<int:fips>/calculatedstats/', methods=['GET'])
+@api.route('counties/<int:fips>/calculatedstats', methods=['GET'])
 @crossdomain(origin='*')
 def calculatedstats_for_county(fips):
     return query.construct_calculatedstats_for_county(fips)
 
 
-@api.route('counties/sssbudget/', methods=['GET'])
+@api.route('counties/sssbudget', methods=['GET'])
 @crossdomain(origin='*')
 def sssbudget_all():
     return query.construct_sssbudget_all()
 
 
-@api.route('counties/<int:fips>/sssbudget/', methods=['GET'])
+@api.route('counties/<int:fips>/sssbudget', methods=['GET'])
 @crossdomain(origin='*')
 def sssbudget_for_county(fips):
     return query.construct_sssbudget_for_county(fips)
 
 
-@api.route('counties/ssscredits/', methods=['GET'])
+@api.route('counties/ssscredits', methods=['GET'])
 @crossdomain(origin='*')
 def ssscredits_all():
     return query.construct_ssscredits_all()
 
 
-@api.route('counties/<int:fips>/ssscredits/', methods=['GET'])
+@api.route('counties/<int:fips>/ssscredits', methods=['GET'])
 @crossdomain(origin='*')
 def ssscredits_for_county(fips):
     return query.construct_ssscredits_for_county(fips)
 
 
-@api.route('counties/ssswages/', methods=['GET'])
+@api.route('counties/ssswages', methods=['GET'])
 @crossdomain(origin='*')
 def ssswages_all():
     return query.construct_ssswages_all()
 
 
-@api.route('counties/<int:fips>/ssswages/', methods=['GET'])
+@api.route('counties/<int:fips>/ssswages', methods=['GET'])
 @crossdomain(origin='*')
 def ssswages_for_county(fips):
     return query.construct_ssswages_for_county(fips)
 
 
-@api.route('counties/puma/', methods=['GET'])
+@api.route('counties/puma', methods=['GET'])
 @crossdomain(origin='*')
 def puma_all():
     return query.construct_puma_all()
 
 
-@api.route('counties/<int:fips>/puma/', methods=['GET'])
+@api.route('counties/<int:fips>/puma', methods=['GET'])
 @crossdomain(origin='*')
 def puma_for_county(fips):
     return query.construct_puma_for_county(fips)
 
 
-@api.route('counties/censushousehold/', methods=['GET'])
+@api.route('counties/censushousehold', methods=['GET'])
 @crossdomain(origin='*')
 def censushousehold_all():
     return query.construct_censushousehold_all()
 
 
-@api.route('counties/<int:fips>/censushousehold/', methods=['GET'])
+@api.route('counties/<int:fips>/censushousehold', methods=['GET'])
 @crossdomain(origin='*')
 def censushousehold_for_county(fips):
     return query.construct_censushousehold_for_county(fips)
